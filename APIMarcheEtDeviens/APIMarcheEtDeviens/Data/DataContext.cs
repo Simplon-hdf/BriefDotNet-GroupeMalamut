0484930ecf072f2b58b7535f72164371b1db3ee2
mikkptr <cacahouettetbs@gmail.com>
2024-08-02 16:27:21 +0200

    feat: additon de la connection à la bdd

---
+++ APIMarcheEtDeviens/APIMarcheEtDeviens/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using APIMarcheEtDeviens.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIMarcheEtDeviens.Data
+{
+	public class DataContext : DbContext
+	{
+		public DbSet<Media> Medias { get; set; }
+		public DbSet<Participer> Participers { get; set; }
+		public DbSet<Randonnee> Randonnees { get; set; }
+		public DbSet<Randonneur> Randonneurs { get; set; }
+		public DbSet<Role> Roles { get; set; }
+		public DbSet<Pensee> Pensees { get; set; }
+
+		public DataContext(DbContextOptions<DataContext> options) : base(options)
+		{
+
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder options)
+		{
+
+		}
+	}
+}
--- APIMarcheEtDeviens/APIMarcheEtDeviens/Program.cs
+++ APIMarcheEtDeviens/APIMarcheEtDeviens/Program.cs
@@ -1,8 +1,17 @@
+using APIMarcheEtDeviens.Data;
+using Microsoft.EntityFrameworkCore;
+using MySql.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+	options.UseMySQL(builder.Configuration.GetConnectionString("DefaultValue"));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
