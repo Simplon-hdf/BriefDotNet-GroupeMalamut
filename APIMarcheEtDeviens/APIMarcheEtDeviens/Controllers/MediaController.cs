b2e2625560cb61b603eba9f84f6cc5d13f7fad5a
mikkptr <cacahouettetbs@gmail.com>
2024-08-02 16:31:18 +0200

    feat: controllers pour Media & Pensees en vue de preparer la migration

---
+++ APIMarcheEtDeviens/APIMarcheEtDeviens/Controllers/MediaController.cs
@@ -0,0 +1,63 @@
+﻿using APIMarcheEtDeviens.Data;
+using APIMarcheEtDeviens.Models;
+using Microsoft.AspNetCore.Mvc;
+using MySql.EntityFrameworkCore;
+
+namespace APIMarcheEtDeviens.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class MediaController : ControllerBase
+	{
+		private readonly DataContext _context;
+		public MediaController(DataContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<List<Pensee>>> GetAllPensees()
+		{
+			var medias = await _context.Medias.ToListAsync();
+
+			return Ok(medias);
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<List<Media>>> AddPensee()
+		{
+			List<Media> medias = new List<Media> {
+				new Media
+				{
+					Id = "1",
+					CheminDuMedia = "",
+					NomMedia = "Media1",
+					TypeMedia = "Photo",
+				},
+				new Media
+				{
+					Id = "2",
+					CheminDuMedia = "",
+					NomMedia = "Media2",
+					TypeMedia = "Video",
+				},
+				new Media
+				{
+					Id = "3",
+					CheminDuMedia = "",
+					NomMedia = "Media3",
+					TypeMedia = "Photo",
+				}
+				};
+
+
+			foreach (Media media in medias)
+			{
+				_context.Medias.Add(media);
+				_context.SaveChanges();
+			}
+
+			return Ok(await _context.Medias.ToListAsync());
+		}
+	}
+}
---
+++ APIMarcheEtDeviens/APIMarcheEtDeviens/Controllers/PenseeController.cs
@@ -0,0 +1,67 @@
+﻿using APIMarcheEtDeviens.Data;
+using APIMarcheEtDeviens.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIMarcheEtDeviens.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class PenseeController : ControllerBase
+	{
+		private readonly DataContext _context;
+		public PenseeController(DataContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<List<Pensee>>> GetAllPensees()
+		{
+			var pensees = await _context.Pensees.ToListAsync();
+
+			return Ok(pensees);
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<List<Pensee>>> AddPensee()
+		{
+			List<Pensee> pensees = new List<Pensee> {
+				new Pensee
+				{
+					Id = "1",
+					NomDeLaPensee = "Pensee 1",
+					ContenuPensee = "Lorem",
+					Date = new DateTime(),
+					Media = null
+				},
+				new Pensee
+				{
+					Id = "2",
+					NomDeLaPensee = "Pensee 2",
+					ContenuPensee = "Ipsum",
+					Date = new DateTime(),
+					Media = null
+				},
+				new Pensee
+				{
+					Id = "3",
+					NomDeLaPensee = "Pensee 3",
+					ContenuPensee = "Dolor",
+					Date = new DateTime(),
+					Media = null
+				}
+				};
+
+
+			foreach (Pensee pensee in pensees)
+			{
+				_context.Pensees.Add(pensee);
+				_context.SaveChanges();
+			}
+
+			return Ok(await _context.Pensees.ToListAsync());
+		}
+
+	}
+}
