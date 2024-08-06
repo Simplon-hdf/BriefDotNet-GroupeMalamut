0456118d4e0bc0b01213a1b2769e2a6160ddc060
mikkptr <cacahouettetbs@gmail.com>
2024-08-06 12:49:06 +0200

    chore: edits in cache file

--- APIMarcheEtDeviens/APIMarcheEtDeviens/obj/Debug/net8.0/APIMarcheEtDeviens.AssemblyInfo.cs
+++ APIMarcheEtDeviens/APIMarcheEtDeviens/obj/Debug/net8.0/APIMarcheEtDeviens.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIMarcheEtDeviens")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93ce9b824c7c177ec7a784850f8e1eb56672c05c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd3e22c45b97d1e90718a11cd40ba4568a031818")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIMarcheEtDeviens")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIMarcheEtDeviens")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
