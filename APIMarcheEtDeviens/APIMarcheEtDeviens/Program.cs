48098c8e27308addd4ab7c4fcc214e68eb020f9d
fat5B <myshop.ino@gmail.com>
2024-08-07 10:01:30 +0200

    gestion merge

--- APIMarcheEtDeviens/APIMarcheEtDeviens/Program.cs
+++ APIMarcheEtDeviens/APIMarcheEtDeviens/Program.cs
@@ -11,6 +11,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddScoped<IController<int, Role>, RoleService>();
 builder.Services.AddScoped<IController<Guid, Randonnee>, RandonneeService>();
+builder.Services.AddScoped<IController<Guid, Randonneur>, RandonneurService>();
+builder.Services.AddScoped<IController<Guid, Pensee>, PenseeService>();
 builder.Services.AddDbContext<DataContext>(options =>
 
 {
