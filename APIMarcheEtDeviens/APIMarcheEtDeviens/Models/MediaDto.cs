85c8c47ff4084918fd674c051fd5adf2aac4b40c
mikkptr <cacahouettetbs@gmail.com>
2024-08-12 14:56:25 +0200

    fix: edits to mapper/models

--- APIMarcheEtDeviens/APIMarcheEtDeviens/Models/MediaDto.cs
+++ APIMarcheEtDeviens/APIMarcheEtDeviens/Models/MediaDto.cs
@@ -4,7 +4,9 @@ namespace APIMarcheEtDeviens.Models
 {
     public class MediaDto
     {
-        public string? CheminDuMedia { get; set; }
+		[Column(TypeName = "nvarchar(128)")]
+		public Guid MediaId { get; set; }
+		public string? CheminDuMedia { get; set; }
 
         [Column(TypeName = "nvarchar(50)")]
         public string? TypeMedia { get; set; }
