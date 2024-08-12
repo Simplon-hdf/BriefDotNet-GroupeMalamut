d540aff33962c53adafc97d333359da3539851ea
mikkptr <cacahouettetbs@gmail.com>
2024-08-12 15:02:35 +0200

    feat: element de router

--- FrontMarcheEtDevenir/src/app/app-routing.module.ts
+++ FrontMarcheEtDevenir/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { CreationOffreComponent } from './creation-offre-page/creation-offre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '/admin/ajoutrandonnee', component: CreationOffreComponent },
+  { path: '/contact', component: ContactPageComponent },
+
+  { path: '**', redirectTo: '/contact' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FrontMarcheEtDevenir/src/app/app.module.ts
+++ FrontMarcheEtDevenir/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { CreationOffreComponent } from './creation-offre-page/creation-offre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    ContactPageComponent,
+    CreationOffreComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
