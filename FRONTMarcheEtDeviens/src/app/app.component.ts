f830354caf3530697240a48b2ad210eec7dcae71
Gabriel Luthun <gabriel.luthun@gmail.com>
2024-08-12 16:07:12 +0200

    feat(app.component): ajout de la partie centrale de l'application

--- FRONTMarcheEtDeviens/src/app/app.component.ts
+++ FRONTMarcheEtDeviens/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CorpsComponent } from './page-accueil/corps/corps.component';
+import { HeaderComponent } from './page-accueil/header/header.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CorpsComponent, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FRONTMarcheEtDeviens';
+  
 }
