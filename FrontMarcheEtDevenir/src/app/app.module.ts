2f895149f69c599bcc864945a0279b3b59fdd61d
mikkptr <cacahouettetbs@gmail.com>
2024-08-13 15:37:05 +0200

    fix: gros fix de la structure de fichier mal portée de l'éditeur de code, problèmes de router et redefinition du app component en standalone

--- FrontMarcheEtDevenir/src/app/app.module.ts
+++ FrontMarcheEtDevenir/src/app/app.module.ts
@@ -5,8 +5,7 @@ import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { ContactPageComponent } from './contact-page/contact-page.component';
-import { CreationOffreComponent } from './creation-offre-page/creation-offre.component';
+
 
 @NgModule({
   declarations: [
@@ -16,8 +15,6 @@ import { CreationOffreComponent } from './creation-offre-page/creation-offre.com
     BrowserModule,
     AppRoutingModule,
     CommonModule,
-    ContactPageComponent,
-    CreationOffreComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- FrontMarcheEtDevenir/src/app/page-accueil/page-accueil.component.ts
+++ FrontMarcheEtDevenir/src/app/page-accueil/page-accueil.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CorpsComponent } from './corps/corps.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'page-accueil',
   standalone: true,
-  imports: [],
+  imports: [CorpsComponent, HeaderComponent],
   templateUrl: './page-accueil.component.html',
   styleUrls: ['./page-accueil.component.css']
 })
