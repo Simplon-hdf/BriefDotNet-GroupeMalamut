b7dabd4822689bbd43bd6ac650a2ecb116b0a14a
fat5B <myshop.ino@gmail.com>
2024-08-14 16:35:50 +0200

    feat : continuation des pages front

--- FrontMarcheEtDevenir/src/app/app-routing.module.ts
+++ FrontMarcheEtDevenir/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConceptComponent } from './concept/concept.component';
+import { OffreRandonneesComponent } from './offre-randonnees/offre-randonnees.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'concept', component: ConceptComponent },
+  { path: 'offreRandonnees', component: OffreRandonneesComponent, }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FrontMarcheEtDevenir/src/app/app.module.ts
+++ FrontMarcheEtDevenir/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConceptComponent } from './concept/concept.component';
+import { OffreRandonneesComponent } from './offre-randonnees/offre-randonnees.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConceptComponent,
+    OffreRandonneesComponent,
   ],
   imports: [
     BrowserModule,
