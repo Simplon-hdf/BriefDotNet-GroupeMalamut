ed06369ce4c5abc16c0eb8cdd6d743d91064703a
mikkptr <cacahouettetbs@gmail.com>
2024-08-19 09:11:27 +0200

    fix: commentaire des regex en attendant de les resoudre

--- APIMarcheEtDeviens/APIMarcheEtDeviens/DTO/RandonneurDTO.cs
+++ APIMarcheEtDeviens/APIMarcheEtDeviens/DTO/RandonneurDTO.cs
@@ -8,13 +8,13 @@ namespace APIMarcheEtDeviens.Services
 		public string Nom { get; set; } = string.Empty;
 		public string Prenom { get; set; } = string.Empty;
 
-		[RegularExpression("[@]", ErrorMessage = "Veuillez entrer un email valide.")]
+		//[RegularExpression("[@]", ErrorMessage = "Veuillez entrer un email valide.")]
 		public string Mail { get; set; } = string.Empty;
 
-		[RegularExpression(@"(?=.{8,})((?=.+\d)|(?=.+\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z])", ErrorMessage = "Le mot de passe doit contenir une majuscule, une minuscule, un chiffre et un caractère spécial")]
+		//[RegularExpression(@"(?=.{8,})((?=.+\d)|(?=.+\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z])", ErrorMessage = "Le mot de passe doit contenir une majuscule, une minuscule, un chiffre et un caractère spécial")]
 		public string MotDePasse { get; set; } = string.Empty;
 
-		[RegularExpression(@"([1-9](?:\d{2}){4})$", ErrorMessage = "Veuillez entrer un numero de téléphone valide.")]
+		//[RegularExpression(@"([1-9](?:\d{2}){4})$", ErrorMessage = "Veuillez entrer un numero de téléphone valide.")]
 		public int? Telephone { get; set; }
 
 		public int RoleId { get; set; } = 1;
