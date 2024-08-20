36a4ee4d2ff6e608adc9df72c4905a3e4a682026
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-08-20 12:25:53 +0200

    feat: ajout du formulaire de connection fonctionnel

--- FrontMarcheEtDevenir/src/app/page-inscription-connection/formulaire-connexion/formulaire-connexion.component.ts
+++ FrontMarcheEtDevenir/src/app/page-inscription-connection/formulaire-connexion/formulaire-connexion.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { ConnectionService } from '../../services/connection.service';
+import { ModuleFormulairesModule } from '../module-formulaires/module-formulaires.module';
+import { NgForm } from '@angular/forms';
 
 @Component({
   standalone : true,
   selector: 'app-formulaire-connexion',
   templateUrl: './formulaire-connexion.component.html',
-  styleUrl: './formulaire-connexion.component.css'
+  styleUrl: './formulaire-connexion.component.css',
+  imports:[ModuleFormulairesModule]
 })
 export class FormulaireConnexionComponent {
+  
+  constructor(public service: ConnectionService){}
+
+  onSubmit(form: NgForm) {
+    this.service.postEnregistrer()
+    .subscribe({
+      next:res=>{
+        console.log(res)
+      },
+      error:err =>{console.log(err)}
+    })
+  }
 
 }
