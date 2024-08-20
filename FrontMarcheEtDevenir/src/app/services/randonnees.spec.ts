84a9ad33ab280dfb36f3a084d56ac1bd6ea70293
Gabriel Luthun <gabriel.luthun@gmail.com>
2024-08-20 09:43:11 +0200

    feat: ajout de la classe 'randonnee'

---
+++ FrontMarcheEtDevenir/src/app/services/randonnees.spec.ts
@@ -0,0 +1,7 @@
+import { Randonnees } from './randonnees';
+
+describe('Randonnee', () => {
+  it('should create an instance', () => {
+    expect(new Randonnees()).toBeTruthy();
+  });
+});
---
+++ FrontMarcheEtDevenir/src/app/services/randonnees.ts
@@ -0,0 +1,14 @@
+export class Randonnees {
+    Date:string = ""
+    Name: string = ""
+    Pays: string = ""
+    Region: string = ""
+    Ville: string = ""
+    PrixTotal: number = 0
+    Duree: number = 0
+    Description: string = ""
+    NombreMinPersonnes: number = 0
+    NombreMaxPersonnes: number = 0
+    DateDeCreation: string = ""
+    DateDeMaj: string = ""
+}
