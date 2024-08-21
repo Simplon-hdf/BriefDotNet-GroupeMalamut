3a97ef5153bf3ee651e6766f32eb143af0178297
Gabriel Luthun <gabriel.luthun@gmail.com>
2024-08-21 12:14:54 +0200

    feat: ajout partie connexion du formulaire de connexion

--- FrontMarcheEtDevenir/src/app/page-inscription-connection/formulaire-connexion/formulaire-connexion.component.ts
+++ FrontMarcheEtDevenir/src/app/page-inscription-connection/formulaire-connexion/formulaire-connexion.component.ts
@@ -1,27 +1,52 @@
-import { Component, Injectable } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
 import { ConnectionService } from '../../services/connection.service';
 import { ModuleFormulairesModule } from '../module-formulaires/module-formulaires.module';
 import { NgForm } from '@angular/forms';
 
 @Component({
-  standalone : true,
+  standalone: true,
   selector: 'app-formulaire-connexion',
   templateUrl: './formulaire-connexion.component.html',
   styleUrl: './formulaire-connexion.component.css',
-  imports:[ModuleFormulairesModule]
+  imports: [ModuleFormulairesModule]
 })
 export class FormulaireConnexionComponent {
-  
-  constructor(public service: ConnectionService){}
+  constructor(public connectionService: ConnectionService) { }
 
-  onSubmit(form: NgForm) {
-    this.service.postEnregistrer()
-    .subscribe({
-      next:res=>{
-        console.log(res)
-      },
-      error:err =>{console.log(err)}
-    })
+  isValidEmail(email: string, form: NgForm): boolean {
+    const valideurUtilisateur: RegExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    return valideurUtilisateur.test(form.value.email!);
   }
 
+  isValidPassword(motDePasse: string, form: NgForm): boolean {
+    const valideurMotDePasse: RegExp = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/g;
+    return valideurMotDePasse.test(form.value.motDePasse!);
+  }
+
+  onSubmit(form: NgForm) {
+    this.connectionService.postLogin(form.value.email, form.value.motDePasse)
+      .subscribe({
+        next: res => {
+          console.log(res);
+        },
+        error: err => {
+          console.log(err);
+        }
+      });
+  }
+
+  onClick(form: NgForm) {
+    if (this.isValidEmail(form.value.email, form) && this.isValidPassword(form.value.motDePasse, form)) {
+      this.connectionService.postLogin(form.value.email, form.value.motDePasse)
+        .subscribe({
+          next: res => {
+            console.log('Connexion réussie', res);
+            // Logique supplémentaire après une connexion réussie
+          },
+          error: err => {
+            console.log('Erreur de connexion', err);
+          }
+        });
+    }
+  }
 }
