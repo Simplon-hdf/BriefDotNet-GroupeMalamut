2716648bd7a2e14719e3c5bc75acd1708d23c67a
ShmPt <cacahouettetbs@gmail.com>
2024-08-21 16:31:46 +0200

    feat: + bouton redirection

--- FrontMarcheEtDevenir/src/app/creation-offre-page/creation-offre/creation-offre.module.ts
+++ FrontMarcheEtDevenir/src/app/creation-offre-page/creation-offre/creation-offre.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+/*pour ajouter un service à un composant standalone, on necessite un passage par module.
+le module recupère les mdules ng, forms et common pour pouvoir les utiliser sur notre composant*/ 
 
 @NgModule({
   declarations: [],
