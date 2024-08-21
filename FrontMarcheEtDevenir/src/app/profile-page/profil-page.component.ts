bc487e0afb8d807b629002156915c599d292a8bd
Gabriel Luthun <gabriel.luthun@gmail.com>
2024-08-21 12:18:52 +0200

    refactor: remove comments to avoid conflicts

--- FrontMarcheEtDevenir/src/app/profile-page/profil-page.component.ts
+++ FrontMarcheEtDevenir/src/app/profile-page/profil-page.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { InformationPersonallesDisplayComponent } from './information-personalles-display/information-personalles-display.component';
+// import { InformationPersonallesDisplayComponent } from './information-personalles-display/information-personalles-display.component';
 import { ChangerMdpComponent } from './changer-mdp/changer-mdp.component';
 
 @Component({
   selector: 'app-profil-page',
   standalone: true,
-  imports: [ InformationPersonallesDisplayComponent, ChangerMdpComponent],
+  // imports: [ InformationPersonallesDisplayComponent, ChangerMdpComponent],
   templateUrl: './profil-page.component.html',
   styleUrl: './profil-page.component.css'
 })
