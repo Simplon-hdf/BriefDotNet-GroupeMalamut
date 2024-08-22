2c1f5866b5981ca25d97e32ab412fc700bc1809b
mikkptr <cacahouettetbs@gmail.com>
2024-08-22 16:45:39 +0200

    feat: le get et ngFor qui marche conffettis et champagne

--- FrontMarcheEtDevenir/src/app/offre-randonnees/offre-randonnees.component.ts
+++ FrontMarcheEtDevenir/src/app/offre-randonnees/offre-randonnees.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { OffresModule } from './offres/offres.module';
+import { ListeOffresService } from '../services/liste-offres.service';
+import { Randonnee } from '../services/randonnee';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   standalone:true,
@@ -10,8 +15,24 @@ import { FooterComponent } from '../footer/footer.component';
   imports: [
     HeaderComponent,
     FooterComponent,
+    OffresModule,
+    CommonModule
   ]
 })
-export class OffreRandonneesComponent {
+@Injectable({ providedIn: 'root' })
 
+export class OffreRandonneesComponent implements OnInit {
+
+  randonnees!: Randonnee[];
+
+  constructor(private service: ListeOffresService) { }
+
+  ngOnInit() {
+    this.service.getRandonnees().subscribe((res) => {
+      this.randonnees = res;
+      console.log(this.randonnees);
+      console.log(this.randonnees[12].name);
+    });
+      return this.randonnees;
+  }
 }
